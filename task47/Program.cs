a5e42aeae34168b4f1a357f06b941eb6ac37ac62
Irina <niko_iv@mail.ru>
2022-08-19 19:54:17 +0300

    done

---
+++ task47/Program.cs
@@ -0,0 +1,21 @@
+Console.Write("Введите количество строк");
+int rows = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите количество столбцов");
+int columns = Convert.ToInt32(Console.ReadLine());
+
+void  GetArray(int m,int n){
+
+    double [,] array = new double[m, n];
+
+    Random random = new Random();
+    for (int rows = 0; rows < m; rows++){
+        for (int columns = 0; columns < n; columns++){
+            array[rows, columns] =random.NextDouble() * 100; 
+            Console.Write($"{array[rows,columns]}");
+        }
+        Console.WriteLine();
+    }
+    
+}
+
+GetArray(rows,columns);
