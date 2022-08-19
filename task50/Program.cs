d359a153cee3cd2b4e70f01ce4b03c48ddcb1c6e
Irina <niko_iv@mail.ru>
2022-08-19 19:47:12 +0300

    done

---
+++ task50/Program.cs
@@ -0,0 +1,30 @@
+﻿Console.Write("Введите строку");
+int rows = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите  столбец");
+int columns = Convert.ToInt32(Console.ReadLine());
+
+int [,]  GetArray(int m,int n){
+
+    int [,] array = new int[m, n];
+
+    Random random = new Random();
+    for (int rows = 0; rows < m; rows++){
+        for (int columns = 0; columns < n; columns++){
+            array[rows, columns] = random.Next(1,10); 
+            Console.Write($"{array[rows,columns]}");
+        }
+        Console.WriteLine();
+    }
+    return array;
+}
+
+void CheckArray(int [,] array,int rows,int columns){
+    if(rows < 0 | rows > array.GetLength(0) | columns < 0 | columns > array.GetLength(1)){
+        Console.WriteLine("такого числа в массиве нет");
+    }else{
+        Console.Write($"{array[rows,columns]}");
+    }
+}
+
+int [,] array = GetArray(3,4);
+CheckArray(array,rows,columns);
